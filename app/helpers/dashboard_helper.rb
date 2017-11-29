e811ead1b5eee0bc5420ea6ad712cb11c0bddc75
Sergey Mazur <Sergey.Mazur@plansource.com>
2017-11-29 10:28:51 -0700

    Fix directory error

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -3,7 +3,7 @@ module DashboardHelper
   def get_avatar(user)
     directory = Redmine::Plugin.find(TeamDashboardConstants::PLUGIN_NAME) rescue nil
     return nil unless directory
-    path_to_avatar = "#{directory&.directory}/assets/images/#{avatar_path(user)}"
+    path_to_avatar = "#{directory.directory}/assets/images/#{avatar_path(user)}"
     if File.exist?(path_to_avatar)
       avatar_path(user)
     else
